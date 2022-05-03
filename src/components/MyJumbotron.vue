ffe1c89ae6ac61667e069e9ca0e49651fe29750d
Penna92 <giacomopenna92@gmail.com>
2022-05-03 16:58:03 +0200

    Creata nuova componente MyJumbotron

---
+++ src/components/MyJumbotron.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img id="jumbotron" src="../assets/jumbotron-image.jpg" alt="Jumbotron image">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyJumbotron',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    #jumbotron {
+        width: 100%;
+        height: 100vh;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
--- src/components/MyTitle.vue
+++ src/components/MyTitle.vue
@@ -1,8 +1,5 @@
 <template>
-    <div id="jumbotron">
-        <h1>La mia prima app con Vue CLI!</h1>
-        <img id="jumbotron" src="../assets/jumbotron-image.jpg" alt="Jumbotron image">
-    </div>
+    <h1>La mia prima app con Vue CLI!</h1>
 </template>
 
 <script>
@@ -17,11 +14,5 @@ export default {
 <style scoped lang="scss">
     h1{
         color: green;
-    }
-    #jumbotron {
-        width: 100%;
-        height: 100vh;
-        object-fit: cover;
-        object-position: center;
     }
 </style>
