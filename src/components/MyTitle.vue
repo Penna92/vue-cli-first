b17dfc35bed91e37aac9374418a0122f943f1453
Penna92 <giacomopenna92@gmail.com>
2022-05-03 16:46:18 +0200

    Prima app con Vue CLI! Scaffolding e aggiunta componente "MyTitle".

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="jumbotron">
+        <h1>La mia prima app con Vue CLI!</h1>
+        <img id="jumbotron" src="../assets/jumbotron-image.jpg" alt="Jumbotron image">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    h1{
+        color: green;
+    }
+    #jumbotron {
+        width: 100%;
+        height: 100vh;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
